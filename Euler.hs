60efb16c23d7ffc46e4bbc2b77c43d786abf0ed2
Colin Woodbury <colingw@gmail.com>
2014-09-02 20:20:13 -0700

    Solved 003 with Haskell

--- Euler.hs
+++ Euler.hs
@@ -5,12 +5,18 @@ module Euler where
 (|:) :: Integral a => a -> a -> Bool
 a |: b = a `mod` b == 0
 
-(\|:) :: Integral a => a -> a -> Bool
-a \|: b = not $ a |: b
+(/|:) :: Integral a => a -> a -> Bool
+a /|: b = not $ a |: b
 
 primes :: Integral a => [a]
 primes = 2 : filter isPrime [3,5..]
 
 isPrime :: Integral a => a -> Bool
-isPrime n = n > 1 && all (n \|:) (takeWhile (<= sqrt' n) primes)
+isPrime n = n > 1 && all (n /|:) (takeWhile (<= sqrt' n) primes)
     where sqrt' n = floor . sqrt $ fromIntegral n
+
+primeFactors :: Integral a => a -> [a]
+primeFactors n = f n primes
+    where f n (p:ps) | n < 2     = []
+                     | n |: p    = p : f (n `div` p) (p:ps)
+                     | otherwise = f n ps
